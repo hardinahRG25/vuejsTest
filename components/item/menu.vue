aaa8dac5e78897638794a51a38f27accfcddbfc6
R_Hardinah <h.rajaonanirina@graphibox.biz>
2024-01-30 10:44:59 +0300

    REFACTOR : routage

--- components/item/menu.vue
+++ components/item/menu.vue
@@ -1,8 +1,11 @@
-<!-- components/Menu.vue -->
 <template>
-    <div>
-      Accueil
-      Liste
-    </div>
-  </template>
-  
+  <div>
+    <h1>Accueil</h1>
+    <p>Bienvenue sur votre application NuxtJS !</p>
+  </div>
+</template>
+
+<script>
+export default {
+};
+</script>
